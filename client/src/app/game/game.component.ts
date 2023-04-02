5b7e7cf759ef3db2a4596009612ba255047377db
ramona1402 <sdramona9@gmail.com>
2023-04-02 18:55:52 +0300

    created the deck of cards

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+interface Card {
+  suit: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
+
 export class GameComponent {
 
+  //creating the deck of 32 cards
+  deck: Card[] = [];
+
+  constructor() {
+    const suits = ['rosu', 'duba', 'verde', 'ghinda'];
+    const values = ['2', '3', '4', '9', '10', 'A'];
+
+    for (const suit of suits) {
+      for (const value of values) {
+        this.deck.push({suit, value});
+      }
+    }
+    console.log(this.deck);
+  }
 }
